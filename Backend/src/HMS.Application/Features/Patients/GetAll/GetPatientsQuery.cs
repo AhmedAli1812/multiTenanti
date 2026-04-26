aa31922d8628ea3a12049da2f96ce2ef47858c5e
FaresGamalMedScope <FaresGamalMedScope@gmail.com>
2026-04-26 04:09:08 +0300

    desplay table of patiant

--- Backend/src/HMS.Application/Features/Patients/GetAll/GetPatientsQuery.cs
+++ Backend/src/HMS.Application/Features/Patients/GetAll/GetPatientsQuery.cs
@@ -7,4 +7,5 @@ public class GetPatientsQuery : IRequest<PaginatedResult<PatientDto>>
     public string? Search { get; set; }
     public int PageNumber { get; set; } = 1;
     public int PageSize { get; set; } = 10;
+    public Guid? TenantId { get; set; }
 }
